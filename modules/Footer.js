35c0a384d89e3130a644fe62abbce70294741159
Paul Serbanescu <61565464+pserb@users.noreply.github.com>
2022-05-23 00:06:42 -0400

    add footer module

---
+++ modules/Footer.js
@@ -0,0 +1,7 @@
+import footer from "../styles/Footer.module.scss"
+
+export default function Footer() {
+    return (
+        <footer className={footer.footer}>Desgined by&nbsp;<a href="https://github.com/pserb">Paul Serbanescu</a></footer>
+    )
+}
