03c11bc136ae7735b1d2a0f2c0881e130639a465
pserb <paulserbanescu3@gmail.com>
2022-08-26 18:31:36 +0000

    Deploying to gh-pages from @ Fission310/fission310.github.io@5b950d71a5c4ad4183ce4cbd48fea1061c90cbe8 🚀

---
+++ _next/static/ytJr-JWk_MeXwPfnVYYtT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/a0f6b063fff497b3.css","static/chunks/pages/index-bfa68397a5d1d309.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
